8b79f946e51a6966db6f7f531f24d662c86c7d4e
Munene <mnene9110@gmail.com>
2026-04-20 09:40:10 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID"
+  apiKey: "AIzaSyDqprWRx0xnOpnPB00Kc8ftEW4Nq24U7hU",
+  authDomain: "matchflow-27524298-12d64.firebaseapp.com",
+  projectId: "matchflow-27524298-12d64",
+  storageBucket: "matchflow-27524298-12d64.firebasestorage.app",
+  messagingSenderId: "469500415820",
+  appId: "1:469500415820:web:f3882b3f9b4d8ffb20c9e9"
 };
 
 /**
